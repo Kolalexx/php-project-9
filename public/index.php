2cb4454a999d3e8db5306d55d029abd4c88524e3
Алексей Колесников <alekseykolesnikov@MacBook-Air-Aleksej.local>
2023-10-10 05:43:30 +0300

    Added handler

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+$app->add(MethodOverrideMiddleware::class);
+
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $response->write('Welcome!');
+});
+
+$app->run();
