c64150df0fa49429fe74c2a31e36a9909b4d46de
Алексей Колесников <you@example.com>
2023-10-22 14:32:15 +0300

    added connection from db

--- public/index.php
+++ public/index.php
@@ -5,6 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 use DI\Container;
 use Slim\Factory\AppFactory;
 use Slim\Middleware\MethodOverrideMiddleware;
+use Carbon\Carbon;
+use Dotenv\Dotenv;
+
+use PostgreSQLTutorial\Connection;
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -18,10 +22,44 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->add(MethodOverrideMiddleware::class);
 
+$router = $app->getRouteCollector()->getRouteParser();
+
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $this->get('renderer')->render($response, "index.phtml");
+    $params = [
+        'name' => [],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, "index.phtml", $params);
 })->setName('main');
+
+$app->post('/', function ($request, $response) use ($router) {
+    $urlData = $request->getParsedBodyParam('url');
+    //!!!!!!$validator = new App\Validator();!!!!!!
+    $errors = [];// $validator->validate($postData)
+    $createdAt = Carbon::now();
+
+    if (count($errors) === 0) {
+        try {
+            $pdo = Connection::get()->connect();
+            echo 'A connection to the PostgreSQL database sever has been established successfully.';
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+        }
+        $sql = "INSERT INTO urls (name, created_at) VALUES (?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$urlData['name'], $createdAt]);
+       // $this->get('flash')->addMessage('success', 'Post has been created');
+        return $response->withRedirect($router->urlFor('main'));
+    }
+
+    $params = [
+        'name' => $urlData,
+        'errors' => $errors
+    ];
+    $response = $response->withStatus(422);
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
+});
 
 $app->run();
